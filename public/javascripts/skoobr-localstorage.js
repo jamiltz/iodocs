0d25b24da511e8fabfaabef72b7f355fa15c436b
James Nocentini <james.nocentini@gmail.com>
2014-08-11 13:18:52 +0100

    add localstorage for credential keys

---
+++ public/javascripts/skoobr-localstorage.js
@@ -0,0 +1,7 @@
+var ls_access_token = localStorage.getItem('access_token');
+var ls_client_key = localStorage.getItem('client_key');
+console.log('hey', ls_access_token)
+if (ls_access_token && ls_client_key) {
+  $('input[name=client_key]').val(ls_access_token);
+  $('input[name=access_token]').val(ls_client_key);
+}
