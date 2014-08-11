8be9835ea6eb93527c0f7542a41610534b217873
James Nocentini <james.nocentini@gmail.com>
2014-08-11 14:04:37 +0100

    add DELETE to unsecuredCall

--- public/javascripts/skoobr-localstorage.js
+++ public/javascripts/skoobr-localstorage.js
@@ -2,6 +2,6 @@ var ls_access_token = localStorage.getItem('access_token');
 var ls_client_key = localStorage.getItem('client_key');
 
 if (ls_access_token && ls_client_key) {
-  $('input[name=client_key]').val(ls_access_token);
-  $('input[name=access_token]').val(ls_client_key);
+  $('input[name=client_key]').val(ls_client_key);
+  $('input[name=access_token]').val(ls_access_token);
 }
